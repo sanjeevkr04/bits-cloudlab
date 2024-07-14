71bf0bf778aa23d52efa83b3e3292fa1e72521e1
Sanjeev <Sanjeevkr4201@gmail.com>
2024-07-14 21:09:42 +0530

    Dockerfile added

--- obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("f9c43c51-6f16-4d0b-8fdd-09c98cbac784")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3036052721f80f55fd8cf92187312db6c2f268c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
